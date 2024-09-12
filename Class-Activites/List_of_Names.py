d53d16ed91ad86553498f4c508227f9b05036c0f
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-09-12 17:40:56 +0000

    Today Work

---
+++ Class-Activites/List_of_Names.py
@@ -0,0 +1,10 @@
+import random
+#create list of random
+
+names = ["laye", "Malcom", "Melany"]
+
+# create possible age range
+ages = list(range(18,60))
+
+print(f"My names is {random.choice(names)}, and I'm {random.choice(ages)} years old.")
+
