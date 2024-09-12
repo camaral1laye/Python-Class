c92dcaab17f2d3b78eae9614b1971a342af47d93
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-09-12 13:35:29 +0000

    Tip Calculator

---
+++ Problem_Set-03/Laye_Camara_Tip_Calculator.py
@@ -0,0 +1,17 @@
+#This program calculate tip and total for a meal at the restaurant 
+print("Tip Calculator\n")
+
+cost_of_meal = float(input("Cost of meal: "))
+tip_percent = float(input("Tip percent: "))
+
+#calculating the tip amount
+tip = cost_of_meal*(tip_percent/100)
+total_amount = cost_of_meal +tip
+
+#display
+print(f"Tip amount: {tip:.2f}")
+print(f"Total amount: $ {total_amount:.2f}")
+
+
+
+#print(f"Gross pay: ${gross_pay:.2}")
