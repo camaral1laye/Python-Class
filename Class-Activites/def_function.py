49630f948255bc5f0bd9d2bc2d31c5e8b4f96ef2
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-09-24 17:54:57 +0000

    update

---
+++ Class-Activites/def_function.py
@@ -0,0 +1,5 @@
+#SaybHi to a user
+def say_hi(x):
+    print(f"Hi {name}.")
+name = input("What is your name: ")
+say_hi(name)
---
+++ Class-Activites/return_function.py
@@ -0,0 +1,6 @@
+#retun function
+def f(x):
+    return x**2 -7
+t = float(input("Enter any number: "))
+y =f(t)
+print(f"f returns {y} when the input is {t}")
