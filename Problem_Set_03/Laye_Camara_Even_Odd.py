ec7b4f74c39642bf9d3d1f753d07e0ee6eb13f2d
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-10-02 17:15:14 +0000

    Laye_Camara_Even_Odd

--- Problem_Set_03/Laye_Camara_Even_Odd.py
+++ Problem_Set_03/Laye_Camara_Even_Odd.py
@@ -1,1 +1,17 @@
-def
+def check_even_or_odd(number):
+    if number % 2 ==0:
+        return "even"
+    else:
+        return "odd"
+    
+def main():
+    print("Even or Odd Checker")
+
+    number = int(input("Enter an integer: "))
+    
+    result = check_even_or_odd(number)
+    
+    print(f"This is an {result} number.")
+    
+if __name__ == "__main__":
+    main()
